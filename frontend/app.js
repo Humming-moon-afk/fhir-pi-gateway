9f37550b2a1da4cbe488601930b8d770d65fea31
Humming-Moon-afk <milian.loechner@gmail.com>
2026-07-22 08:59:43 +0200

    feat(frontend): convert from submit data into JSON payload

--- frontend/app.js
+++ frontend/app.js
@@ -4,5 +4,13 @@ const patientForm = document.getElementById('PatientForm');
 
 patientForm.addEventListener('submit', function(event) {
     event.preventDefault();
+
+    const formData = new FormData(patientForm);
+    const dataObject = Object.fromEntries(formData);
+
+    const patientJSON = JSON.stringify(dataObject, null, 2);
+
+
+    console.log(patientJSON);
 });
 
