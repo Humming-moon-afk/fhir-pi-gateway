ff05ee4d8b84555cbb12e7e2b1b1fc5019135909
Humming-Moon-afk <milian.loechner@gmail.com>
2026-07-23 16:27:34 +0200

    feat(db): add PatientEntity for database mapping

---
+++ backend/src/main/java/com/med/backend/PatientEntity.java
@@ -0,0 +1,61 @@
+package com.med.backend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.Date;
+@Entity
+public class PatientEntity {
+    @Id
+    String ID;
+    String firstName;
+    String lastName;
+    String gender;
+    Date birthDate;
+    String managingClinic;
+    String sonstiges;
+    public PatientEntity() {
+        
+    }
+    public String getID() {
+        return ID;
+    }
+    public void setID(String iD) {
+        ID = iD;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public Date getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    public String getManagingClinic() {
+        return managingClinic;
+    }
+    public void setManagingClinic(String managingClinic) {
+        this.managingClinic = managingClinic;
+    }
+    public String getSonstiges() {
+        return sonstiges;
+    }
+    public void setSonstiges(String sonstiges) {
+        this.sonstiges = sonstiges;
+    }
+}
